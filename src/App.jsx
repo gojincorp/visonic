3d12bbca8efdb92820f33e4b73927634f6c39759
Gojincorp <gojincorp@hotmail.com>
2020-01-13 23:00:48 -0800

    Workaround for source-map issue.  Source directory must be one level deep from root...

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "core-js/stable"                 // Replacement for deprecated @babel-polyfill
+import "regenerator-runtime/runtime"    // Replacement for deprecated @babel-polyfill
+import $ from 'jquery'
+import Popper from 'popper.js'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import IssueList from './IssueList.jsx' // Custom module
+
+$(function() {
+const contentNode = $('#contents')[0]
+
+ReactDOM.render(<IssueList />, contentNode)
+})
+
+if (module.hot) {
+    module.hot.accept()
+}
+//TEST
